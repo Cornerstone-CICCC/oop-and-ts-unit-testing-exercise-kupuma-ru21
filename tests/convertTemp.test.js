2acebdfc87776fac099409f4dd0219fc118999b0
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-09-20 13:39:52 -0700

    feat: done

--- tests/convertTemp.test.js
+++ tests/convertTemp.test.js
@@ -3,6 +3,14 @@
 // Test case 2: Converting 104 deg fahrenheit will return 40 deg celsius
 // Both test cases need to pass
 
-const Converter = require('../src/convertTemp')
+const Converter = require("../src/convertTemp");
 
-const convert = new Converter()
+const convert = new Converter();
+
+test("Converting 30°C to Fahrenheit should return 86°F", () => {
+  expect(convert.convertToFahrenheit(30)).toBe(86);
+});
+
+test("Converting 104°F to Celsius should return 40°C", () => {
+  expect(convert.convertToCelsius(104)).toBe(40);
+});
--- tests/counter.test.js
+++ tests/counter.test.js
@@ -4,4 +4,23 @@
 // Both test cases need to pass
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
-const counter = require('../src/counter')
+const counter = require("../src/counter");
+
+const parameter = 10;
+const timeout = (parameter + 1) * 1000;
+
+test(
+  'counter(10, "increment") will eventually return 10',
+  () => {
+    return expect(counter(parameter, "increment")).resolves.toBe(10);
+  },
+  timeout
+);
+
+test(
+  'counter(10, "decrement") will eventually return 0',
+  () => {
+    return expect(counter(parameter, "decrement")).resolves.toBe(0);
+  },
+  timeout
+);
